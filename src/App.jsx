bc723a2dde730383acb3458d014a4a48dafec2ac
AGUACATE-ONLINE <alan.castillo.e@gmail.com>
2024-11-27 19:09:05 +0100

    Start creating and modify css from nav bar, create folder structure for other stuf from the repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
+import { 
+  background, 
+  black_shadow, 
+  logo, 
+  sun_rays, 
+  fog_1, fog_2, 
+  mountain_1, mountain_2 
+} from './assets/images/landingPage/index.js';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World!</h1>
+    <header>
+      <nav>
+        <img src={logo} alt='Logo' className='logo'/>
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">PROJECT</a></li>
+          <li><a href="#">CURRICULUM</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+      </nav>
+      
+    </header>
     </>
   )
 }
-
 export default App
